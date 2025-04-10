ac29189e46920e4757f5cb28bb6dbdaa8fa5059e
atik65 <atikhasan801@gmail.com>
2025-04-10 22:05:07 +0600

    feat: scan and c-scan disk scheduling algorithm implementation in python

---
+++ lab6/c-scan/c-scan.py
@@ -0,0 +1,50 @@
+# C Scan Disk Sheduling Algorithm
+
+def scan_disk_sheduling(request_sequence, initial_head):
+    current_head = initial_head
+    sequence = []
+    total_seektime = 0
+    
+    request_sequence = sorted(request_sequence)
+    
+    left = [r for r in request_sequence if r < current_head]
+    right = [r for r in request_sequence if r >= current_head]
+    
+    # left.reverse()
+
+    for request in  right + left:
+        total_seektime += abs(request - current_head)
+        sequence.append(request)
+        current_head = request      
+    
+    return total_seektime, sequence
+
+   
+def take_input():
+    head = int(input("Enter the initial head position: "))
+    n = int (input("Enter the total number of sequene: "))
+    req_sequences = []
+
+    for i in range(n):
+        req = int(input(f'Enter sequence number  {i+1} : '))
+        req_sequences.append(req)
+    
+    return req_sequences, head
+    
+
+def print_sequence(sequences):
+    for i in range(len(sequences)):
+        if i < len(sequences)-1:
+            print(sequences[i], end=" ---> ")
+        else: 
+            print(sequences[i], end="")
+         
+
+if __name__ == "__main__":
+    inputs = take_input()
+    req_sequences = inputs[0]
+    head = inputs[1]
+   
+    res = scan_disk_sheduling(req_sequences, head)
+    print("Total Seek ope Operation", res[0])
+    print_sequence(res[1])
